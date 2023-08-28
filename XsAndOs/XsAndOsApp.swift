4172fb03fe876809196120403cd6cb6dae8a0535
ssss <samil>
2023-08-28 04:49:18 -0700

    Initial Commit

---
+++ XsAndOs/XsAndOsApp.swift
@@ -0,0 +1,17 @@
+//
+//  XsAndOsApp.swift
+//  XsAndOs
+//
+//  Created by samil on 28.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct XsAndOsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
